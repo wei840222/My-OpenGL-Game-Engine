ec1b740c1b37c757bc2298afabc47a53c0e02ce7
萬俊瑋 <wei840222@outlook.com>
2016-06-03 22:02:40 +0800

    碰撞偵測Demo

--- My OpenGL Game Engine/Main.cpp
+++ My OpenGL Game Engine/Main.cpp
@@ -8,92 +8,96 @@ int old_x = 0, old_y = 0;
 GLfloat spin_x = 0, spin_y = 0;
 
 Color red(1, 0, 0), green(0, 1, 0), blue(0, 0, 1);
-Point o(0, 0, 0);
-Vector u(1, 2, 3), v(10, 10, 10);
+Point O(0, 0, 0), cube(0, 20, 0);
+Vector N(0, 1, 0), V(0, 40, 0);
+Line L(O, V);
+Plane P(O, N);
 
-Vector V[3] = { { 1, 2, 3 },{ 4, 5, 6 },{ 7, 8, 9 } };
-Matrix3x3 M(V[0], V[1], V[2]);
-
+////////////////初始化////////////////
 void Initialize() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	glOrtho(-20, 20, -20, 20, -20, 20);
+	glOrtho(-50, 50, -50, 50, -50, 50);
 	glMatrixMode(GL_MODELVIEW);
+}
+
+////////////////顯示計算結果////////////////
+void ShowData() {
+	system("CLS");
+	cout << "cube(" << cube.x << ", " << cube.y << ", " << cube.z << ")" << endl;
+	cout << "CollisionPoint(" << collisionPoint(L, P).x << ", " << collisionPoint(L, P).y << ", " << collisionPoint(L, P).z << ")" << endl;
 }
 
 ////////////////描繪畫面////////////////
 void Display() {
-	glClearColor(0, 0, 0, 1);				//不透明黑色背景
+	glClearColor(1, 1, 1, 1);				//不透明黑色背景
 	glClear(GL_COLOR_BUFFER_BIT);			//清理顏色緩衝區
 
+											//畫面旋轉
 	glPushMatrix();
 	glTranslatef(-0.5, 0, 0);
 	glRotatef(spin_y, 1.0, 0.0, 0.0);
 	glRotatef(spin_x, 0.0, 1.0, 0.0);
 
-	//繪圖
-	Line lu(o, u), lv(o, v), luvCN(o, cross(u, v).normalize());
-	lu.draw(red);
-	lv.draw(green);
-	luvCN.draw(blue);
 
-	glutWireCube(20);
+	//平面
+	glBegin(GL_QUADS);
+	glColor3f(1, 0, 0);
+	glVertex3f(10, 0, 10);
+	glVertex3f(-10, 0, 10);
+	glVertex3f(-10, 0, -10);
+	glVertex3f(10, 0, -10);
+	glEnd();
 
+	//線
+	L.draw(green);
+
+	//方塊
+	glPushMatrix();
+	glColor3f(0, 0, 1);
+	glTranslatef(0, cube.y, 0);
+	glutSolidCube(1);
 	glPopMatrix();
-	glFlush();								//更新畫面
+
+	//更新畫面
+	glPopMatrix();
+	glFlush();
 }
 
 ////////////////獲取滑鼠按下////////////////
 void MouseClick(int button, int state, int x, int y) {
 	old_x = x;
 	old_y = y;
-	Display();
+	glutPostRedisplay();
 }
 
 ////////////////獲取滑鼠拖曳////////////////
 void MouseMotion(int x, int y) {
 	spin_x = x - old_x;
 	spin_y = y - old_y;
-	Display();
+	glutPostRedisplay();
 }
 
-////////////////顯示計算結果////////////////
-void ShowData() {
-	//向量運算值
-	cout << "Practice 1: " << u.length() << endl;
-	cout << "length u: " << u.length() << endl;
-	cout << "length v: " << v.length() << endl;
-	cout << "normalize u: " << u.normalize().i << ' ' << u.normalize().j << ' ' << u.normalize().k << endl;
-	cout << "normalize v: " << v.normalize().i << ' ' << v.normalize().j << ' ' << v.normalize().k << endl;
-	cout << "dot product u.v: " << dot(u, v) << endl;
-	cout << "cross product uxv: " << cross(u, v).i << ' ' << cross(u, v).j << ' ' << cross(u, v).k << endl;
-	//矩陣運算值
-	cout << "\nPractice 2: " << endl;
-	cout << "M = " << endl;
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3; j++)
-			cout << M.element[i][j] << ' ';
-		cout << endl;
-	}
-	cout << "M cofactor (1,0) = " << M.cofactor(1, 0) << endl;
-	cout << "M's det = " << M.det() << endl;
-	if (M.det() == 0)
-		cout << "M is singular !" << endl;
-	else {
-		Matrix3x3 Mi = M.inverse();
-		cout << "M is invertible !" << endl;
-		cout << "M's inverse = " << endl;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++)
-				cout << Mi.element[i][j] << ' ';
-			cout << endl;
-		}
+////////////////獲取鍵盤按下////////////////
+void SpecialKeyPress(int key, int x, int y) {
+	switch (key) {
+	case GLUT_KEY_UP:
+		if (cube.y < 40)
+			cube.y++;
+		glutPostRedisplay();
+		ShowData();
+		break;
+	case GLUT_KEY_DOWN:
+		if (cube.y != collisionPoint(L, P).y)
+			cube.y--;
+		glutPostRedisplay();
+		ShowData();
+		break;
 	}
 }
-
 
 int main() {
-	glutInitWindowSize(400, 300);					//視窗長寬
+	glutInitWindowSize(800, 600);					//視窗長寬
 	glutInitWindowPosition(400, 300);				//視窗左上角的位置
 	glutCreateWindow("My OpenGL Projects");			//建立視窗並打上標題
 
@@ -102,6 +106,7 @@ int main() {
 	//下面三個與Callback函式有關
 	glutMouseFunc(MouseClick);						//滑鼠按下
 	glutMotionFunc(MouseMotion);					//滑鼠拖曳
+	glutSpecialFunc(SpecialKeyPress);				//處理按鍵
 	glutDisplayFunc(Display);						//負責描繪
 	glutMainLoop();									//進入主迴圈
 	return 0;
